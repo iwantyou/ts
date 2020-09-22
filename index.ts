111b98dae0f93642f192b81f47cd0c5b9b98bb58
duzibo <duzb@asiainfo.com>
2020-09-22 13:13:10 +0800

    d

--- index.ts
+++ index.ts
@@ -2,11 +2,11 @@
 import express = require('express')
 import { router } from './router'
 
-let PORT:number = 3000
-let HOST:string = "localhost" 
+let PORT:number = 3001
+let HOST:string = "118.89.234.184" 
 const app: express.Application = express()
 
-app.use('/api', router)
+app.use('/', router)
 app.listen(PORT, HOST, () => {
     console.log('app running ...')
 })
--- router/index.ts
+++ router/index.ts
@@ -2,13 +2,19 @@ import express = require('express')
 
 const router: express.Router = express.Router()
 
-router.use('/', (req:express.Request, res:express.Response) => {
-    if(req.url === "/"){
+router.use('/api', (req:express.Request, res:express.Response) => {
+ 
         res.json({
             status: 200,
             data: '124'
         })
-    }
 }) 
+router.use('/index', (req:express.Request, res:express.Response) => {
+     res.setHeader('content-Type', "text/html")
+     res.json({
+        status: 200,
+        data: '<h1>12555</h1>'
+    })
+})
 
 export { router }
